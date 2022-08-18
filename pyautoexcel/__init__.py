6736a741927aeece08570dd35e048fedc57bd8fa
wn0x00 <320753691@qq.com>
2022-08-18 20:42:31 +0800

    feat: add save and close method

--- pyautoexcel/__init__.py
+++ pyautoexcel/__init__.py
@@ -1,11 +1,25 @@
-import os
 import win32com.client
 
 
 class Excel(object):
-    def __init__(self):
-        self._excel = win32com.client.Dispatch("Excel.Application")
-        self._excel.Visible = 1
+    def __init__(self, filename=None):
+        self.application = win32com.client.Dispatch("Excel.Application")
+        self.application.Visible = 1
+        if filename:
+            self.filename = filename
+            self.workbook = self.application.Workbooks.Open(filename)
+        else:
+            self.filename = ""
+            self.workbook = self.application.Workbooks.Add()
 
-    def open(self, filename: str) -> None:
-        self._excel.Workbooks.Open(filename)
+
+    def save(self, new_filename):
+        if new_filename:
+            self.filename = new_filename
+            self.workbook.SaveAs(new_filename)
+        else:
+            self.workbook.Save()
+
+    def close(self):
+        self.workbook.Close(SaveChanges=0)
+        del self.application
